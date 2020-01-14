4d4eab37eae056edb031a1e4dcdad270fcef35c6
Thien Dang <sydroux00@gmail.com>
2020-01-14 18:57:39 -0500

    New programs

---
+++ dang_01/src/dang_01/FirstProgram.java
@@ -0,0 +1,12 @@
+package dang_01;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+			int age = 13;
+			double weight = 123.456789;
+			System.out.printf("Hello!%n"); 
+			System.out.printf("I am %d years old.%n", age);
+			System.out.printf("I weight %.2f pounds.", weight);
+	}
+}
---
+++ dang_01/src/dang_01/SecondProgram.java
@@ -0,0 +1,10 @@
+package dang_01;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
