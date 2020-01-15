3fc4b84b4816c2c8f422416b2e3c0b9692b66546
Thien Dang <sydroux00@gmail.com>
2020-01-14 19:09:29 -0500

    first commit

--- dang_01/src/dang_01/FirstProgram.java
+++ dang_01/src/dang_01/FirstProgram.java
@@ -7,6 +7,13 @@ public class FirstProgram {
 			double weight = 123.456789;
 			System.out.printf("Hello!%n"); 
 			System.out.printf("I am %d years old.%n", age);
-			System.out.printf("I weight %.2f pounds.", weight);
+			System.out.printf("I weight %.2f pounds.%n", weight);
+			
+			int x = 12;
+			int y = 10;
+			double u = 10.0;
+			System.out.println(x / 0.001);
+			System.out.println(y / u);
+			System.out.println(10.0 % 12.0);
 	}
 }
